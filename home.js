096929530b37370a5173fd27e856fa6a9debfb9e
SushruthMurakare <sushruthraomurakare@gmail.com>
2025-12-02 16:29:21 -0700

    favicon, removing unwanted code and images, CV, appointment google form embedding

--- home.js
+++ home.js
@@ -5,57 +5,57 @@ fetch("home.json")
     document.getElementById("title").textContent = data.title;
     document.getElementById("desc").textContent = data.overview;
 
-    const postsContainer = document.getElementById("postsContainer");
+    // const postsContainer = document.getElementById("postsContainer");
 
-    data.posts.forEach((post) => {
-      const card = document.createElement("div");
-      card.className = "post-card";
+    // data.posts.forEach((post) => {
+    //   const card = document.createElement("div");
+    //   card.className = "post-card";
 
-      const link = document.createElement("a");
-      link.href = post.link;
-      link.target = "_blank";
-      link.className = "post-link";
+    //   const link = document.createElement("a");
+    //   link.href = post.link;
+    //   link.target = "_blank";
+    //   link.className = "post-link";
 
-      const header = document.createElement("div");
-      header.className = "post-header";
+    //   const header = document.createElement("div");
+    //   header.className = "post-header";
 
-      const headerImg = document.createElement("div");
-      headerImg.className = "headerImg";
+    //   const headerImg = document.createElement("div");
+    //   headerImg.className = "headerImg";
 
-      const headshot = document.createElement("img");
-      headshot.src = "./headshot.jpeg";
+    //   const headshot = document.createElement("img");
+    //   headshot.src = "./headshot.jpeg";
 
-      const headerNameTitle = document.createElement("div");
-      headerNameTitle.className = "headerNameTitle";
+    //   const headerNameTitle = document.createElement("div");
+    //   headerNameTitle.className = "headerNameTitle";
 
-      const name = document.createElement("h4");
-      name.textContent = data.name;
+    //   const name = document.createElement("h4");
+    //   name.textContent = data.name;
 
-      headerNameTitle.appendChild(name);
+    //   headerNameTitle.appendChild(name);
 
-      const title = document.createElement("h5");
-      title.textContent = data.title;
+    //   const title = document.createElement("h5");
+    //   title.textContent = data.title;
 
-      headerNameTitle.appendChild(title);
+    //   headerNameTitle.appendChild(title);
 
-      headerImg.appendChild(headshot);
+    //   headerImg.appendChild(headshot);
 
-      header.appendChild(headerImg);
-      header.appendChild(headerNameTitle);
+    //   header.appendChild(headerImg);
+    //   header.appendChild(headerNameTitle);
 
-      const img = document.createElement("img");
-      img.src = post.image;
-      img.alt = post.desc;
+    //   const img = document.createElement("img");
+    //   img.src = post.image;
+    //   img.alt = post.desc;
 
-      const desc = document.createElement("p");
-      desc.textContent = post.desc;
+    //   const desc = document.createElement("p");
+    //   desc.textContent = post.desc;
 
-      card.appendChild(header);
-      card.appendChild(img);
-      card.appendChild(desc);
+    //   card.appendChild(header);
+    //   card.appendChild(img);
+    //   card.appendChild(desc);
 
-      link.appendChild(card);  
+    //   link.appendChild(card);  
 
-      postsContainer.appendChild(link);
-    });
+    //   postsContainer.appendChild(link);
+    // });
   });
